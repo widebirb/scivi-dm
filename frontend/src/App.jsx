08b44c7372fa80676f1611045b24ed6bcf1fb3fd
widebirbe <bridhone@gmail.com>
2026-03-09 10:52:29 +0800

    feat: initialize frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900">
+            <h1 className="text-8xl font-bold text-white">Tailwind works</h1>
+        </div>
+    )
+}
+
+export default App
